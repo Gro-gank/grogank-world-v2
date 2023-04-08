8b401910c97520e82b1daa774b1cf85a95f40891
Thang <grogankick@gmail.com>
2023-04-09 01:39:05 +0700

    up

--- src/components/mainpage/TheBanner.vue
+++ src/components/mainpage/TheBanner.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="full-screen">
-    <video
+  <div class="full-screen md:h-[30rem] w-full sm:h-[10rem]">
+    <!-- <video
       autoplay
       muted
       loop
       src="video/banner3.mp4"
       class="video-overlay mt-[-1rem] opacity-20"
-    ></video>
+    ></video> -->
+    <img src="images/bg2.jpg" alt="" class="h-full w-full opacity-40" />
     <div
       class="text-4xl font-bold text-center justify-center md:py-[10rem] md:h-[100%] md:w-[95%] sm:absolute sm:top-[10rem] sm:left-5 z-[5]"
     >
